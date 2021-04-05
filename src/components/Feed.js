d48476ce9ec603779d84ecef42ac9f0de4503670
PC <lsongulija@gmail.com>
2021-04-05 15:57:19 +0200

    Get all tweets from firestore real time

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Feed.css'
 import Post from './Post'
 import TweetBox from './TweetBox'
+import db from '../firebase.js'
 
 function Feed() {
+
+    //useState initial value & function to update initial value. by default empty array
+    const [posts, setPosts] = useState([])
+
+    //triggers when Feed component loads up
+    useEffect(() => {
+        //go to firestore 'posts' collection. onSpanshot is basically listens to database
+        //for any changes(data deletes inserts, updates ..). it basically grabs that snapshot and gives it 
+        //to you. and code bellow runs
+        db.collection('posts').onSnapshot(snapshot => {
+            //setting posts. use snapshot (actual database documents ). get all docs that came back in that snapshot
+            //map through every doc(document) and get doc.data(all those fields of document)
+            setPosts(snapshot.docs.map(doc => doc.data()))//so this 'll give you array of all post documents in db
+        })
+    }, [])
+    //if one of these vars is changed it will trigger useEffect method
+
     return (
         <div className='feed'>
             <div className='feed__header'>
                 <h2>Home</h2>
             </div>
 
-        {/* TweetBox */}
-        <TweetBox />
+            {/* TweetBox */}
+            <TweetBox />
 
-        {/* POSTS */}
-        <Post displayName='Sunny Sangha' username='sssanga' verified={true} text='Yooo its working' avatar='https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png' image='https://media3.giphy.com/media/65ATdpi3clAdjomZ39/giphy.gif'/>
-        
-        </div>   
+            {/* POSTS. loop through posts array and get every single post. And for each build Post component
+            pass post properties values*/}
+            {posts.map((post) => (
+                <Post displayName={post.displayName} username={post.username}
+                    verified={post.verified} text={post.text} avatar={post.avatar}
+                    image={post.image} />
+            ))}
+
+
+        </div>
 
     )
 }
